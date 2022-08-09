4fe1b97bd25d6669ea4de53742c4fd0b4fe9e4c9
ravinderchautala <ryvinder@gmail.com>
2022-08-09 15:47:41 +0530

    tylor series tutorial completed

---
+++ ch02_recursion-ps/tylor-series.js
@@ -0,0 +1,57 @@
+function tylor(x, n) {
+    let p = 1
+    let f = 1
+    let r
+    if (n == 0) {
+        return 1;
+    } else {
+        r = tylor(x, n - 1)
+        p = p * x
+        f = f * n
+        return r + p / f
+    }
+    return 0;
+}
+console.log(tylor(1, 10))
+
+function e(x, n) {
+    let p = 1
+    let f = 1
+    let r
+    if (n == 0) {
+        return 1
+    }
+    r = e(x, n - 1)
+    p = p * x
+    f = f * n
+    return r + p / f
+}
+console.log(e(1, 10))
+// using harmor's rule
+
+// function har(x, n) {
+//     let s
+//     if(n == 0) {
+//         return s
+//     } else {
+//         s = 1 + x * s / n
+//         return har(x, n - 1)
+//     }
+//     return 0;
+// }
+// console.log(har(1, 10))
+
+// using loops
+
+function loop(x,n){
+    let s=1
+    let num =1
+    let den=1
+    for(let i=1;i<n;i++){
+        num*=x
+        den*=i
+        s+=num/den
+    }
+    return s
+}
+console.log(loop(1,10))
