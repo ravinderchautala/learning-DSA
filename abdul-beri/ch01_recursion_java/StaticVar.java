91bb5d6e4c95664133fe7277fbe0c9dcc28fa85b
ravinderchautala <ryvinder@gmail.com>
2022-09-09 19:08:03 +0530

    recursion revision

---
+++ abdul-beri/ch01_recursion_java/StaticVar.java
@@ -0,0 +1,19 @@
+package ch01_recursion_java;
+
+
+// Static Variable
+public class StaticVar {
+    static int x = 0;
+    static int fun(int n){
+            if(n>0){
+                x++;
+                return fun(n-1)+x;
+            }
+            return 0;
+    }
+    public static void main(String[] args) {
+    int r;
+    r=fun(5);
+    System.out.println(r);
+    }
+}
---
+++ abdul-beri/ch01_recursion_java/TreeRecursion.java
@@ -0,0 +1,16 @@
+package ch01_recursion_java;
+
+public class TreeRecursion {
+    static int fun(int n){
+        if(n>0){
+            System.out.print(" "+n);
+            fun(n-1);
+            fun(n-1);
+        }
+       return 0;
+    }
+    public static void main(String[] args) {
+        int x = 3;
+        fun(x);
+    }
+}
